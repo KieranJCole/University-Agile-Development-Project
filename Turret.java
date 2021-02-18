0b7b15d2c7fffad6f8653477958ea36d17d0d8c6
George <ga312@kent.ac.uk>
2021-02-18 16:45:07 +0000

    Added the shot delay, tank will now fire once every 5 seconds

--- Turret.java
+++ Turret.java
@@ -8,15 +8,13 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Turret extends Player
 {
-<<<<<<< HEAD
-=======
     /**
      * Act - do whatever the Turret wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     
     private int direction, speed;
->>>>>>> b60b42bc05f450a61f05c110c6abdbcf45c37785
+    private int shotTimer;
     Player tank;
     
     public Turret(Player tank)
@@ -32,9 +30,11 @@ public class Turret extends Player
             MouseInfo mp = Greenfoot.getMouseInfo();
             turnTowards(mp.getX(), mp.getY());
         }
-        if (Greenfoot.isKeyDown("space"))
+        if (Greenfoot.isKeyDown("space") && (shotTimer == 0)) //When Space is pressed the tank will fire if the Shot Timer is equal to 0
         {
             getWorld().addObject(new Shell(getRotation()), getX(), getY());
+            shotTimer = 200; //Currently the timer is set to roughly 5 seconds
         }
+        if (shotTimer > 0) shotTimer --; //If the Shot Timer is greater than 0 it will -1 for this act cycle.
     }
 }
