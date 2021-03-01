72c1d226e4401d6d19c2d0cf22f41d5291133752
Daniel Essex <de224@kent.ac.uk>
2021-03-01 09:12:09 +0000

    healthBar values and parameters

--- healthBar.java
+++ healthBar.java
@@ -14,12 +14,24 @@ public class healthBar extends Player
     private int endHealth = 0; //player eliminated
     private int value = 0; // to add and subtract health from player
     
+    private int barWidth = 100; //Width of Player's Health Bar
+    private int barHeight = 10; //Height of Player's Health Bar
+    //^(Subject to change)
     
+    private boolean colorChange = false; //
+    private boolean changeColo
     private Color textColor = Color.BLACK; // shows value of how much health the player has
     private Color greenHealth = Color.GREEN; // Health color til health gets down to 40
     private Color orangeHealth = Color.ORANGE;// Health color til health gets down to 20
     private Color redHealth = Color.RED;// Health color til player is destroyed.
     
+    
+    
+    
+    
+    /*public Bar(String refText, String unitType, int initValue, int maxValue) 
+     * referenceText = 
+    */
     public void hit(int amount)
     {
         value -= amount;
