f29300f343d5774e767935f71177ede0844119c7
Daniel Essex <de224@kent.ac.uk>
2021-04-07 14:20:09 +0100

    Powerup behaviour and Inventory class

---
+++ Inventory.java
@@ -0,0 +1,27 @@
+public class Inventory extends Actor 
+{
+    String[]Inventory;
+
+    boolean speed = false;
+    boolean health = false;
+    boolean fireRate = false;
+    boolean damage = false;
+
+    //Player pick up, turn statement to true
+    if speed = true
+    {
+        speed = 1.35;  
+    }
+
+    if health = true
+    {
+        health +=25;
+    }
+    if fireRate = true
+    {
+        fireRate = 2
+    }
+    if damage = true
+    {
+        damage = 10;
+    }
